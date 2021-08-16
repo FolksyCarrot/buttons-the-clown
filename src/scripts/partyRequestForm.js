7b8bcff8e635848f86b962b4bad51773ec1053fa
Keith Parks <keithparks20@gmail.com>
2021-08-16 14:03:09 -0500

    updated click event so the postPartyRequest was called inside the click event, vs before I had it outside the click event function itself. It was retained inside the first parenthesis of the click event, essentially saying it was a parameter

--- src/scripts/partyRequestForm.js
+++ src/scripts/partyRequestForm.js
@@ -2,32 +2,31 @@ import { postPartyForm } from "./dataAccess.js"
 
 const mainContainer = document.querySelector("#mainContainer")
 
-// document.addEventListener("click", 
-//   (event) => {
-//     if(event.target.id === "partyRequestButton") {
-//       const parentNameData = document.querySelector("input[name=`parentName']").value
-//       const childNameData = document.querySelector("input[name=`childName']").value
-//       const guestNumbersData = document.querySelector("input[name=`guestNumbers']").value
-//       const addressOfPartyData = document.querySelector("input[name=`addressOfParty']").value
-//       const dateOfPartyData = document.querySelector("input[name=`dateOfParty']").value
-//       const lengthOfPartyData = document.querySelector("input[name=`lengthOfParty']").value
+document.addEventListener("click", 
+  (event) => {
+    if(event.target.id === "partyRequestButton") {
+      const parentNameData = document.querySelector("input[name=`parentName']").value
+      const childNameData = document.querySelector("input[name=`childName']").value
+      const guestNumbersData = document.querySelector("input[name=`guestNumbers']").value
+      const addressOfPartyData = document.querySelector("input[name=`addressOfParty']").value
+      const dateOfPartyData = document.querySelector("input[name=`dateOfParty']").value
+      const lengthOfPartyData = document.querySelector("input[name=`lengthOfParty']").value
 
 
-//       const partyRequestData = {
-//         parentName: parentNameData,
-//         childName: childNameData,
-//         numOfChildren: guestNumbersData,
-//         address: addressOfPartyData,
-//         date: dateOfPartyData,
-//         lengthOfPartyInHours: lengthOfPartyData
-//       }
-//     }
+      const partyRequestData = {
+        parentName: parentNameData,
+        childName: childNameData,
+        numOfChildren: guestNumbersData,
+        address: addressOfPartyData,
+        date: dateOfPartyData,
+        lengthOfPartyInHours: lengthOfPartyData
+      }
 
-//     postPartyForm(partyRequestData)
+      postPartyForm(partyRequestData)
+    }
 
-
-//   }
-// )
+  }
+)
 
 export const renderHtml = () => {
   return `<h1>Request A Party</h1><div>
